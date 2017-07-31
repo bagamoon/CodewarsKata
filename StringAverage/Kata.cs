405170bbc7fbf28dbb831748f4ee4c292f4f02a4
brett.yu <brett.yu@titansoft.com.sg>
2017-07-31 23:05:51 +0800

    initial commit

---
+++ StringAverage/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAverage
+{
+    public class Kata
+    {
+        public string AverageString(string input)
+        {
+            return "n/a";
+        }
+
+    }
+}
---
+++ TestProject/KataTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using StringAverage;
+using Assert = NUnit.Framework.Assert;
+
+namespace TestProject
+{
+    [TestFixture]
+    public class KataTest
+    {
+        [TestCase("", "n/a", TestName = "Input_Empty_String_Should_Return_n/a")]
+        public void AverageStringTest(string input, string expected)
+        {
+            var kata = new Kata();
+
+            var actual = kata.AverageString(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
