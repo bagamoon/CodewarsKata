0989ab546862383fdb78b4b4938fdb9de9aa232e
brett.yu <brett.yu@titansoft.com.sg>
2017-07-31 23:46:35 +0800

    add the rest test case on codewars

--- StringAverage/Enums.cs
+++ StringAverage/Enums.cs
@@ -13,5 +13,10 @@ namespace StringAverage
         two = 2,
         three = 3,
         four = 4,
+        five = 5,
+        six = 6,
+        seven = 7,
+        eight = 8,
+        nine = 9
     }
 }
--- TestProject/KataTest.cs
+++ TestProject/KataTest.cs
@@ -13,6 +13,12 @@ namespace TestProject
         [TestCase("one three", "two", TestName = "Input_Divisible_Total_Should_Return_Correct_Result")]
         [TestCase("one two", "one", TestName = "Input_Nondivisible_Total_Should_Return_Floor_Result")]
         [TestCase("four", "four", TestName = "Input_One_Num_Should_Return_Same_Result")]
+        [TestCase("zero nine five two", "four")]
+        [TestCase("four six two three", "three")]
+        [TestCase("one two three four five", "three")]
+        [TestCase("five four", "four")]
+        [TestCase("zero zero zero zero zero", "zero")]
+        [TestCase("one one eight one", "two")]
         public void AverageStringTest(string input, string expected)
         {
             AssertAverage(input, expected);            
