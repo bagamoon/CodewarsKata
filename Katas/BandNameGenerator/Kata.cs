fa2176be3e5bb6b3421523274590e160b70086a5
brett.yu <brett.yu@titansoft.com.sg>
2017-08-12 00:37:28 +0800

    add BandNameGenerator and test case

---
+++ Katas/BandNameGenerator/Kata.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.BandNameGenerator
+{
+	public class Kata
+	{
+		public string BandNameGenerator(string name)
+		{
+			if (IsFirstAndLastCharAreDifferent(name))
+				return string.Format("The {0}", FormatName(name, true));
+
+			return string.Format("{0}{1}", FormatName(name, true), FormatName(name, false));
+		}
+
+		private bool IsFirstAndLastCharAreDifferent(string name)
+		{
+			return name.Length == 1 ||
+			       name.First() != name.Last();
+		}
+
+		private string FormatName(string name, bool isNeedFirstChar)
+		{
+			var firstChar = name.First().ToString().ToUpper();
+			var theRestPart = name.Substring(1).ToLower();
+
+			return isNeedFirstChar ? string.Format("{0}{1}", firstChar, theRestPart) : theRestPart;
+		}
+	}
+}
---
+++ TestProject/BandNameGenerator/KataTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Katas.BandNameGenerator;
+using NUnit.Framework;
+
+namespace TestProject.BandNameGenerator
+{
+	[TestFixture]
+	public class KataTest
+	{
+		[TestCase("Knife", "The Knife", TestName = "Input_Different_Starts_Ends_Should_Prefix_The")]
+		[TestCase("banana", "The Banana", TestName = "Input_Different_Starts_Ends_and_Lower_Char_Should_Prefix_The_and_Upper_First_Char")]
+		[TestCase("KUMA", "The Kuma", TestName = "Input_Different_Starts_Ends_and_Upper_Char_Should_Prefix_The_and_Upper_Only_First_Char")]
+		[TestCase("tart", "Tartart", TestName = "Input_Same_Starts_Ends_Should_Repeat_Twice_and_Upper_First_Char")]
+		[TestCase("SOS", "Sosos", TestName = "Input_Same_Starts_Ends_and_Upper_Char_Should_Repeat_Twice_and_Upper_Only_First_Char")]
+		[TestCase("X", "The X", TestName = "Input_Only_One_Char_Should_Prefix_The")]
+		public void BandNameGeneratorTest(string input, string expected)
+		{
+			AssertBandNameGenerator(input, expected);
+		}
+
+		private void AssertBandNameGenerator(string input, string expected)
+		{
+			var kata = new Kata();
+			var actual = kata.BandNameGenerator(input);
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
