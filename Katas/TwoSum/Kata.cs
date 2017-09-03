d4747c0b934ff0b0b0f643daac538fc2f79e7373
brett.yu <brett.yu@titansoft.com.sg>
2017-09-03 21:15:17 +0800

    add leetcode TwoSum and Testcases

---
+++ Katas/TwoSum/Kata.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.TwoSum
+{
+	public class Kata
+	{
+		public int[] TwoSum(int[] nums, int target)
+		{
+			var dict = nums.Select((p, i) => new {Index = i, Value = p})
+								.GroupBy(p => p.Value)
+								.ToDictionary(p => p.Key, p => p.First().Index);
+
+			for (int i = 0; i < nums.Length; i++)
+			{
+				int diff = target - nums[i];
+				int pairIndex;
+				if (dict.TryGetValue(diff, out pairIndex) && pairIndex != i)
+				{
+					return i < pairIndex ? new int[] {i, pairIndex} : new int[] {pairIndex, i};
+				}
+			}
+
+			return new int[] {-1, -1};
+		}
+	}
+}
---
+++ TestProject/TwoSum/KataTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Katas.TwoSum;
+using NUnit.Framework;
+
+namespace TestProject.TwoSum
+{
+	[TestFixture]
+	public class KataTest
+	{
+		[Test]
+		public void Input_Two_Items_And_Their_Sum_As_Target_Should_Retun_0_1()
+		{
+			var input = new int[] {4, 7};
+			var target = 11;
+			var expected = new int[] {0, 1};
+
+			AssertTwoSum(input, target, expected);
+		}
+
+		[Test]
+		public void Input_Duplicate_Items_And_Their_Sum_As_Target_Should_Retun_0_1()
+		{
+			var input = new int[] { 3, 3 };
+			var target = 6;
+			var expected = new int[] { 0, 1 };
+
+			AssertTwoSum(input, target, expected);
+		}
+
+		[Test]
+		public void Input_Item_And_Its_Double_As_Target_Should_Return_Correct_Indexs()
+		{
+			var input = new int[] { 3, 2, 4 };
+			var target = 6;
+			var expected = new int[] { 1, 2 };
+
+			AssertTwoSum(input, target, expected);
+		}
+
+		private void AssertTwoSum(int[] input, int target, int[] expected)
+		{
+			var kata = new Kata();
+
+			var actual = kata.TwoSum(input, target);
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
